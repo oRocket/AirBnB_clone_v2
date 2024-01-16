bf2f785fb6b9169064f24a64a892bcab17598327
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-01-16 13:58:53 -0800

    hot fix

--- models/place.py
+++ models/place.py
@@ -80,3 +80,4 @@ class Place(BaseModel, Base):
         def amenities(self, obj=None):
             """ Appends amenity ids to the attribute """
             if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
